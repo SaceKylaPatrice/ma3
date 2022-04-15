60014e1a86203eca4331747cd931b9b3141e3407
SaceKylaPatrice <kylapatricesoberanosace@gmail.com>
2022-04-15 01:58:36 -1200

    Song4 added

---
+++ Song4.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Areuminct-Lovers Music Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Ginintuang Tanawin</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Ikaw Ang Musika</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Kahel Na Langit</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Ikaw Ang Pahinga Ko</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Marupok</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-2 text-center">
+                <h1 class="fw-bolder">Welcome to Areuminct-Lovers Music Lyrics</h1>
+                <p class="lead">“This is inspired with the wattpad stories  written by Areuminct. Music is your own experience, your thoughts, your wisdom. If you don’t live it, it won’t come out of your horn.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-2">
+                <div class="row gx-6 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Ikaw Ang Pahinga Ko</h2>";
+                        echo "<p class='lead '>Cacai Colmenar</p>";
+                        echo "<br/>";
+
+                        //1st stanza
+                        echo "<p> I:</p>";
+                        echo "<p> Nagsimula lahat dahil sayong kaarawan</p>";
+                        echo "<p> Hindi inaasahang Maisasayaw sa harapan</p>";
+                        echo "<p> Galit na galit ka pa, pero aminin mo</p>";
+                        echo "<p> Ginusto mo naman</p>";
+
+                        echo "<br/>";
+
+                        
+                        //2nd stanza
+                        echo "<p> II:</p>";
+                        echo "<p> Tuwang tuwa ako kapag ikay inaasar</p>";
+                        echo "<p> Pinipilosopo ang bawat salitang binabagsak</p>";
+                        echo "<p> Katulad ng</p>";
+                        echo "<p> 'What? Watawat'</p>";
+                        echo "<p> 'Ha?hattitude ka ghorl'</p>";
+
+                        echo "<br/>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus:</p>";
+                        echo "<p> 'Di inakalang magkakasundo </p>";
+                        echo "<p> Ikaw pala ang Pahinga ko </p>";
+                        echo "<p> Ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Madalas kumain ng sabay sa umaga </p>";
+                        echo "<p> Minsan magaway parang aso at Pusa</p>";
+                        echo "<p> Aah aah</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pero kahit ganun</p>";
+                        echo "<p> Ikaw parin ang Bibe ko</p>";
+
+                        echo "<br/>";
+
+                        echo "<p> Ooh</p>";
+
+                        echo "<br/>";
+
+
+                        echo "<p> Sinusuportahan ang bawat isa</p>";
+                        echo "<p> Tumatalon, Sumisigaw sa tuwa</p>";
+                        echo "<p> Aah aah</p>";
+                        echo "<p> Di magbabago</p>";
+                        echo "<p> Ikaw ang pahinga ko</p>";
+                        echo "<br/>";
+                        
+                        //3rd stanza
+                        echo "<p> III:</p>";
+                        echo "<p> Bawat kilos ko sadyang ikaw naaalala ko </p>";
+                        echo "<p> Nawawala ang pagod pag ikaw ng ang kausap ko</p>";
+                        echo "<p> Bakit ganon?</p>";
+                        echo "<p> Hulog na hulog sayo</p>";
+
+                        echo "<br/>";
+
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus:</p>";
+                        echo "<p> 'Di inakalang magkakasundo </p>";
+                        echo "<p> Ikaw pala ang Pahinga ko </p>";
+                        echo "<p> Ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Madalas kumain ng sabay sa umaga </p>";
+                        echo "<p> Minsan magaway parang aso at Pusa</p>";
+                        echo "<p> Aah aah</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pero kahit ganun</p>";
+                        echo "<p> Ikaw parin ang Bibe ko</p>";
+
+                        echo "<br/>";
+
+                        echo "<p> Ooh</p>";
+                        echo "<p> Sinusuportahan ang bawat isa</p>";
+                        echo "<p> Tumatalon, Sumisigaw sa tuwa</p>";
+                        echo "<p> Aah aah</p>";
+                        echo "<p> Di magbabago</p>";
+                        echo "<p> Ikaw ang pahinga ko</p>";
+                        echo "<br/>";
+
+                        //bridge
+                        echo "<br/>";
+                        echo "<p> Pangako sayo</p>";
+                        echo "<p> Ikaw ang pahinga ko</p>";
+                        
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kyla Patrice Soberano Sace </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
